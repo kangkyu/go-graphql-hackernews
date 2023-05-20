4ed40b646bb40c800acf486e754a14c34d06ad16
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-05-20 16:41:22 -0700

    Wip - add bcrypt and hash password

--- go.mod
+++ go.mod
@@ -6,14 +6,14 @@ require (
 	github.com/99designs/gqlgen v0.17.31
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/go-sql-driver/mysql v1.7.1
-	github.com/golang-migrate/migrate v3.5.4+incompatible
+	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/golang-migrate/migrate/v4 v4.15.2
 	github.com/vektah/gqlparser/v2 v2.5.1
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 )
 
 require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
-	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
--- internal/users/users.go
+++ internal/users/users.go
@@ -1,7 +1,57 @@
 package users
 
+import (
+	"database/sql"
+	"log"
+
+	database "github.com/kangkyu/hackernews/internal/pkg/db/mysql"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	ID       string
 	Username string
 	Password string
+}
+
+func (user *User) Create() {
+	stmt, err := database.Db.Prepare("INSERT INTO Users (Username, Password) VALUES (?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	hashedPassword, err := HashPassword(user.Password)
+	_, err = stmt.Exec(user.Username, hashedPassword)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func (user *User) Authenticate() bool {
+	stmt, err := database.Db.Prepare("SELECT Password from Users WHERE Username = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	row := stmt.QueryRow(user.Username)
+
+	var hashedPassword string
+	err = row.Scan(&hashedPassword)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false
+		} else {
+			log.Fatal(err)
+		}
+	}
+	return CheckPasswordHash(user.Password, hashedPassword)
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
